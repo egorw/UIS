e161e932cdacbb434be2b9e516774be07e51781f
Egor Yarmolenko <egor.w@ukr.net>
2018-09-07 18:07:42 +0300

    Capitalize

---
+++ TestProject/src/lesson2/CapitalizeLetter.java
@@ -0,0 +1,28 @@
+package lesson2;
+
+public class CapitalizeLetter {
+
+	public static void main(String[] args) {
+		String source1 = "as gtet E rt w wi ywe Aa try";
+		System.out.println(capitalize(source1));
+		
+		String source2 = "ay yY ttt ghthhh ";
+		System.out.println(capitalize(source2));
+
+	}
+	
+	static String capitalize(String str) {
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if ("aeiou".indexOf(ch) != -1) {
+				result.append(Character.toUpperCase(ch));
+			}
+			else {
+				result.append(ch);
+			}
+		}
+		return result.toString();
+	}
+
+}
